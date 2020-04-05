8fbca6b810a94d84bea54408663c88ef394c87d8
HinaMaeyama <hinahina117mae@gmail.com>
2020-04-05 18:54:32 +0900

    [add]4章

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def full_title(page_titel='')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_titel.empty?
+            base_title
+        else
+            page_titel +  " | " + base_title
+        end
+    end
+
 end
